7a50f4bbcee894d91aad1b74714f94a41ff39973
박선영 <haert2010@naver.com>
2024-03-11 22:18:36 +0900

    [study] JSX 문법 3가지 - JSX 문법1. class는 className - JSX 문법2. 변수는 {중괄호} - JSX 문법3. css 스타일도 {중괄호} -- css 는 object 형태로 사용({ {color:'red} }
    commit by sy.park

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  // 중괄호 문법 => 변수로 선언 후 {} 사용
+  let post = '강남 우동 맛집';
+  //자바스크립트의 경우 :  document.querySelector('h4').innerHTML = post;
+  // JSX 문법1. class 넣을 땐 className
+  // JSX 문법2. 데이터바인딩은 {중괄호}
+  // JSX 문법3. style넣을 땐 style={ {css style 넣기} }
+  // css 스타일은 object 형태로 넣어야 함. 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 id={post} style={ {color:'red'} }>블로그</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
